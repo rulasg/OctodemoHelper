deb480e0c7e11c6a0ad28b43daef67bfb453fa30
rulasg <rulasg@github.com>
2024-11-15 15:56:08 +0100

    disable issue functions

--- Test/public/OctodemoIssues.test.ps1
+++ Test/public/OctodemoIssues.test.ps1
@@ -1,11 +1,11 @@
-function Test_OctodemoIssues_Dev{
+# function Test_OctodemoIssues_Dev{
 
 
-    $number = 68 ; $owner = "octodemo" ; $repo = "bootstrap"
+#     $number = 68 ; $owner = "octodemo" ; $repo = "bootstrap"
 
-    Set-MockInvokeCommandAlias -Command "gh issue delete $number -R $owner/$repo -y" -Mock "echo DELETED_ISSUE_69"
+#     Set-InvokeCommandMock -Alias "gh issue delete $number -R $owner/$repo -y" -Command "echo DELETED_ISSUE_69"
 
-    $result = Remove-OctodemoRepo -Number 69
+#     $result = Remove-OctodemoRepo -Number 69
 
-    Assert-AreEqual -Expected "DELETED_ISSUE_69" -Presented $result
-}
+#     Assert-AreEqual -Expected "DELETED_ISSUE_69" -Presented $result
+# }
--- public/OctodemoIssues.ps1
+++ public/OctodemoIssues.ps1
@@ -1,42 +1,42 @@
 
-Set-MyInvokeCommandAlias -Alias deleteIssue -Command "gh issue delete {number} -R {owner}/{repo} -y"
+# Set-MyInvokeCommandAlias -Alias deleteIssue -Command "gh issue delete {number} -R {owner}/{repo} -y"
 
-function Get-OctodemoIssue {
-    [CmdletBinding()]
-    param()
+# function Get-OctodemoIssue {
+#     [CmdletBinding()]
+#     param()
 
-    $jsonOutput = gh issue list -R octodemo/bootstrap -A rulasg --json number,url,labels,title
-    $json = $jsonOutput | ConvertFrom-Json
+#     $jsonOutput = gh issue list -R octodemo/bootstrap -A rulasg --json number,url,labels,title
+#     $json = $jsonOutput | ConvertFrom-Json
 
-    $issues = $json | ForEach-Object {
-        [PSCustomObject]@{
-            Title  = $_.title
-            Number = $_.number
-            URL    = $_.url
-            Labels = ($_.labels | ForEach-Object { $_.name }) -join ', '
-        }
-    }
-    return $issues
-} Export-ModuleMember -Function Get-OctodemoIssue
+#     $issues = $json | ForEach-Object {
+#         [PSCustomObject]@{
+#             Title  = $_.title
+#             Number = $_.number
+#             URL    = $_.url
+#             Labels = ($_.labels | ForEach-Object { $_.name }) -join ', '
+#         }
+#     }
+#     return $issues
+# } Export-ModuleMember -Function Get-OctodemoIssue
 
-function Remove-OctodemoIssue {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [int]$Number
-    )
+# function Remove-OctodemoIssue {
+#     [CmdletBinding(SupportsShouldProcess = $true)]
+#     param(
+#         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+#         [int]$Number
+#     )
 
-    process {
-        $params = @{
-            Number = $Number
-            Owner  = $OCTIDEMO_ORG
-            Repo   = $BOOTSTRAP_REPO
-        }
+#     process {
+#         $params = @{
+#             Number = $Number
+#             Owner  = $OCTIDEMO_ORG
+#             Repo   = $BOOTSTRAP_REPO
+#         }
 
-        if ($PSCmdlet.ShouldProcess("Issue #$Number", "Delete")) {
-            # gh issue delete $Number -R octodemo/bootstrap -y
-            Invoke-MyCommand -Command deleteIssue -Parameters $params
-        }
-    }
-} Export-ModuleMember -Function Remove-OctodemoIssue
+#         if ($PSCmdlet.ShouldProcess("Issue #$Number", "Delete")) {
+#             # gh issue delete $Number -R octodemo/bootstrap -y
+#             Invoke-MyCommand -Command deleteIssue -Parameters $params
+#         }
+#     }
+# } Export-ModuleMember -Function Remove-OctodemoIssue
 
